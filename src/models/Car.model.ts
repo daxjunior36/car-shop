6b8cff9446a1c81b49e4a875cd9f336cbd993d24
Domingos Alexandre <domingos_alexandre@d.alexandre.jrInspiron-3501>
2022-09-19 10:55:20 -0300

    req_13 possibilidade  atualizar o registro de um carro através do seu id

--- src/models/Car.model.ts
+++ src/models/Car.model.ts
@@ -10,13 +10,12 @@ const carMongooseShema = new Schema<ICar>({
   buyValue: Number,
   doorsQty: Number,
   seatsQty: Number,
-});
-// }, { versionkey: false });
+}, { versionKey: false });
 
 export default class Car extends MongoModel<ICar> {
-  static read() {
-    throw new Error('Method not implemented.');
-  }
+  // static read() {
+  //   throw new Error('Method not implemented.');
+  // }
   constructor(model = mongooseCreateModel('Car', carMongooseShema)) {
     super(model);
   }
